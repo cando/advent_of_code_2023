d384dc2ea075d7e2b1c54b41a3a1ae039e1e989c
Stefano <stefano.candori@prima.it>
2023-12-03 22:54:57 +0100

    day2 part 1

---
+++ app/Day2.hs
@@ -0,0 +1,74 @@
+module Day2 (execute) where
+
+import Control.Monad (join, void)
+import Data.Void
+import Debug.Trace
+import System.IO
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+type Parser = Parsec Void String
+
+data Color = Red Int | Green Int | Blue Int
+
+execute :: IO Int
+execute = do
+  file <- openFile "app/inputs/day2.txt" ReadMode
+  sum . map handleGame . lines <$> hGetContents file
+  where
+    handleGame s = case runParser pGame "" s of
+      Left a -> error (errorBundlePretty a)
+      Right n -> n
+
+-- Part 1
+pGame :: Parser Int
+pGame = do
+  gids <- string "Game " *> some numberChar <* char ':'
+  let gid = read gids :: Int
+  rounds <- join <$> some pRound
+  let (b, r, g) =
+        foldl
+          ( \(a1, a2, a3) el -> case el of
+              Blue n -> (n <= 14 && a1, a2, a3)
+              Red n -> (a1, a2 && n <= 12, a3)
+              Green n -> (a1, a2, a3 && n <= 13)
+          )
+          (True, True, True)
+          rounds
+  let num = if b && r && g then gid else 0
+  return $ trace (show gid ++ ": " ++ show num) num
+
+-- pGame :: Parser Int
+-- pGame = do
+--   gids <- string "Game " *> some numberChar <* char ':'
+--   let gid = read gids :: Int
+--   rounds <- join <$> some pRound
+--   let (b, r, g) =
+--         foldl
+--           ( \(a1, a2, a3) el -> case el of
+--               Blue n -> (a1 + n, a2, a3)
+--               Red n -> (a1, a2 + n, a3)
+--               Green n -> (a1, a2, a3 + n)
+--           )
+--           (0, 0, 0)
+--           rounds
+--   let num = if b <= 14 && r <= 12 && g <= 13 then gid else 0
+--   return $ trace (show gid ++ ": " ++ show num) num
+
+pRound :: Parser [Color]
+pRound =
+  do
+    some
+      ( try (pColor Blue "blue")
+          <|> try (pColor Red "red")
+          <|> try (pColor Green "green")
+          <|> ( Red 0
+                  <$ char ','
+              )
+      )
+    <* (void (char ';') <|> eof)
+
+pColor :: (Int -> Color) -> String -> Parser Color
+pColor c s = do
+  n <- spaceChar *> some numberChar <* space <* string s
+  return $ c (read n :: Int)
--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,13 @@
 module Main where
 
 import Day1 qualified
+import Day2 qualified
 
 main :: IO ()
 main =
   do
     doDay 1 Day1.execute
+    doDay 2 Day2.execute
   where
     doDay :: Int -> IO Int -> IO ()
     doDay n day = do
