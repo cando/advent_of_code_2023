cd309bff84b981645bc6e07b0d727d7ddf8ea969
Stefano <stefano.candori@prima.it>
2023-12-07 23:19:38 +0100

    Day2 Part 2!

--- app/Day7.hs
+++ app/Day7.hs
@@ -3,10 +3,10 @@ module Day7 where
 import Data.Bifunctor (Bifunctor (first), bimap)
 import Data.Function (on)
 import Data.List (sort, sortBy)
-import Data.List.Extra (group, nub)
+import Data.List.Extra (group)
 import System.IO
 
-data Card = A | K | Q | J | T | N9 | N8 | N7 | N6 | N5 | N4 | N3 | N2 | N1 deriving (Ord, Eq, Show)
+data Card = A | K | Q | T | N9 | N8 | N7 | N6 | N5 | N4 | N3 | N2 | J deriving (Ord, Eq, Show)
 
 data Hand = FiveOfAKind [Card] | FourOfAKind [Card] | FullHouse [Card] | ThreeOfAKind [Card] | TwoPair [Card] | OnePair [Card] | HighCard [Card]
   deriving (Ord, Eq, Show)
@@ -15,28 +15,27 @@ execute :: IO (Int, Int)
 execute = do
   file <- openFile "app/inputs/day7.txt" ReadMode
   ll <- lines <$> hGetContents file
-  let cards = map parseForPart1 ll
-  return (part1 cards, 0)
+  let cards = map parseForPart2 ll
+  return (part2 cards, 0)
 
-parseForPart1 :: String -> ([Card], Int)
-parseForPart1 = bimap (map toCard) read . toTuple . words
+parseForPart2 :: String -> ([Card], Int)
+parseForPart2 = bimap (map toCard) read . toTuple . words
 
-part1 :: [([Card], Int)] -> Int
-part1 = sum . zipWith (*) [1 ..] . map snd . sortBy (flip compare `on` fst) . map (first detectHand)
+part2 :: [([Card], Int)] -> Int
+part2 = sum . zipWith (*) [1 ..] . map snd . sortBy (flip compare `on` fst) . map (first handType)
 
-detectHand :: [Card] -> Hand
-detectHand cards
-  | all (== head cards) cards = FiveOfAKind cards
-  | (length . filter (\g -> length g == 4) . group . sort) cards == 1 = FourOfAKind cards
-  | (length . group . sort) cards == 2 = FullHouse cards
-  | (length . filter (\g -> length g == 3) . group . sort) cards == 1 = ThreeOfAKind cards
-  | (length . filter (\g -> length g == 2) . group . sort) cards == 2 = TwoPair cards
-  | (length . nub) cards == 4 = OnePair cards
-  | (length . nub) cards == 5 = HighCard cards
-  | otherwise = error $ show cards
-
-part2 :: Int
-part2 = 0
+handType :: [Card] -> Hand
+handType cards =
+  case sort $ map length $ group $ sort $ filter (/= J) cards of
+    [] -> FiveOfAKind cards
+    [_] -> FiveOfAKind cards
+    [1, _] -> FourOfAKind cards
+    [2, _] -> FullHouse cards
+    [1, 1, _] -> ThreeOfAKind cards
+    [1, 2, _] -> TwoPair cards
+    [1, 1, 1, _] -> OnePair cards
+    [1, 1, 1, 1, 1] -> HighCard cards
+    _ -> undefined
 
 toTuple [hand, bid] = (hand, bid)
 toTuple _ = error "input error"
@@ -54,5 +53,4 @@ toCard '5' = N5
 toCard '4' = N4
 toCard '3' = N3
 toCard '2' = N2
-toCard '1' = N1
 toCard _ = error "no card"
