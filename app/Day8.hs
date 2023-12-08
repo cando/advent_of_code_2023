b55ea453d5cc789863e662c12d6e9b4f626224ff
Stefano <stefano.candori@prima.it>
2023-12-09 00:24:28 +0100

    Day 8 Part2, not working

--- app/Day8.hs
+++ app/Day8.hs
@@ -2,6 +2,7 @@ module Day8 where
 
 import Control.Arrow ((&&&))
 import Control.Monad (foldM)
+import Data.List (isSuffixOf)
 import Data.Map qualified as M
 import Data.Maybe (fromJust)
 import Data.Void
@@ -17,7 +18,7 @@ execute = do
   file <- openFile "app/inputs/day8.txt" ReadMode
   ll <- lines <$> hGetContents file
   let input = parseForPart1 ll
-  return (part1 input, 0)
+  return (part2 input, 0)
 
 parseForPart1 :: [String] -> (String, M.Map String (String, String))
 parseForPart1 = (cycle . head) &&& parseGraph
@@ -31,15 +32,19 @@ parseForPart1 = (cycle . head) &&& parseGraph
       right <- count 3 anySingle <* string ")"
       return (key, (left, right))
 
-part1 :: (String, M.Map String (String, String)) -> Int
-part1 (directions, graph) =
-  length $
-    takeWhile (/= "ZZZ") $
-      scanl
-        ( \node dir ->
-            let curNode = fromJust $ M.lookup node graph
-             in -- dCurNode = trace (show curNode) curNode
-                if dir == 'L' then fst curNode else snd curNode
-        )
-        "AAA"
-        directions
+part2 :: (String, M.Map String (String, String)) -> Int
+part2 (directions, graph) =
+  let endingsA = filter (isSuffixOf "A") $ M.keys graph
+   in length $
+        takeWhile (not . all (isSuffixOf "Z")) $
+          scanl
+            ( \nodes dir ->
+                map
+                  ( \node ->
+                      let curNode = fromJust $ M.lookup node graph
+                       in if dir == 'L' then fst curNode else snd curNode
+                  )
+                  nodes
+            )
+            endingsA
+            directions
